28757b903c7f0651b6709887e218ae2245214278
diego <2095333@talnet.nl>
2020-09-11 23:42:47 +0200

    Kleine aanpassing in de HTML

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 <fieldset>
             <h3>Login</h3>
             <form action="login.php" method="post">
-                <label for="uname">Username:</label><br> <!-- required="required"-->
+                <label for="username">Username:</label><br> <!-- required="required"-->
                 <input pattern="[a-z]{1,15}" required type="text"  name="username"><br> <!-- required="required"-->
-                <label for="pword" >Password:</label><br>
+                <label for="password" >Password:</label><br>
                 <input required minlength="10" maxlength="20" type="password" name="password">
                 <br><br>
                 <button type="submit" name="Login" value="Login">Login</button><br>
