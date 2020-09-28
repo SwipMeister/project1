cae420483b8e73cce7251599942186abd935c01b
diego <2095333@talnet.nl>
2020-09-28 15:23:50 +0200

    reemail toegevoegd als name voor bevestiging

--- lostpsw.php
+++ lostpsw.php
@@ -14,8 +14,8 @@
         <form action="restorepw.php" method="post">
             <label for="email">email:</label><br> <!-- required="required"-->
             <input required type="email"  name="email"><br> <!-- required="required"-->
-            <label for="email">Bevestig email:</label><br> <!-- required="required"-->
-            <input required type="email"  name="email"><br> <!-- required="required"-->
+            <label for="reemail">Bevestig email:</label><br> <!-- required="required"-->
+            <input required type="email"  name="reemail"><br> <!-- required="required"-->
             <button type="submit" name="recover" value="recover">Recover</button><br>
             <a href="signup.php">Heb je nog geen account? Registreer nu</a><br>
             <a href="index.php">Terug naar het begin</a><br>
