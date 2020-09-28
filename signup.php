2a614dc3be7d73ad4c7f5e2f07d84dc43b31564a
diego <2095333@talnet.nl>
2020-09-28 14:32:30 +0200

    object instantiated

--- signup.php
+++ signup.php
@@ -1,9 +1,20 @@
 <?php
     
-    include 'database.php';
+    include 'db.php';
 
-    $db = new database('localhost', 'root', '', 'project1', 'utf8');
-    $db->executeQueryExample;
+    $db = new db('localhost', 'project1', 'root', '',  'utf8');
+    // $db->executeQueryExample;
+
+    // todo check op isset
+    $voornaam = $_POST['voornaam'];
+    $tussenvoegsel = $_POST['tussenvoegsel'];
+    $achternaam = $_POST['achternaam'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $repassword = $_POST['repassword'];
+
+    $db->insertAccount($email, $password);
 
 ?>
 
@@ -16,11 +27,11 @@
 <body>
 <fieldset>
             <h3>Sign-up</h3>
-            <form action="register.php" method="post">
+            <form action="signup.php" method="post">
                 <label for="voornaam">Voornaam:</label><br> <!-- required="required"-->
                 <input required type="text" name="voornaam" id="voornaam" ><br> <!-- required="required"-->
                 <label for="tussenvoegsel" >Tussenvoegsel:</label><br>
-                <input required type="text" name="tussenvoegsel" id="tussenvoegsel" placeholder="Optioneel"><br>
+                <input type="text" name="tussenvoegsel" id="tussenvoegsel" placeholder="Optioneel"><br>
                 <label for="achternaam" >Achternaam:</label><br>
                 <input required type="text" name="achternaam" id="achternaam"><br>
                 <label for="email">email:</label><br> <!-- required="required"-->
@@ -30,11 +41,10 @@
                 <label for="password" >Password:</label><br>
                 <input required minlength="10" maxlength="20" type="password" name="password" id="password"><br>
                 <label for="repassword" >Repeat Password:</label><br>
-                <input required minlength="10" maxlength="20" type="password" name="password" id="password"><br>
+                <input required minlength="10" maxlength="20" type="password" name="repassword" id="password"><br>
                 <br>
                 <button type="submit" name="Register" value="Register">Register</button><br>
                 <a href="index.php">Terug naar het begin</a><br>
-                <p>test</p>
             </form>
 </fieldset>
 </body>
