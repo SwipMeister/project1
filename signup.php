41602a5706756a6da9fee036eb977a387e7eac91
diego <2095333@talnet.nl>
2020-10-05 13:17:19 +0200

    kleine aanpassing foutcodes weggewerkt moet nog getest worden

--- signup.php
+++ signup.php
@@ -14,56 +14,64 @@
     // $password = $_POST['password'];
     // $repassword = $_POST['repassword'];
 
-    $db->insertAccount($email, $password);
+    $voornaam = $achternaam = $email = $username = $password = "";
+    $voornaamErr = $achternaamErr = $emailErr = $usernameErr = $passwordErr = $pwMatch = $success = "";
+
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["voornaam"])) {
-          $voornaamErr = "Voornaam is verplicht";
-        } else {
-          $voornaam = ($_POST["voornaam"]);
-        }
-        
-        if (empty($_POST["achternaam"])) {
-          $achternaamErr = "Achternaam is verplicht";
-        } else {
-          $achternaam = ($_POST["achternaam"]);
-        }
-        
-        if (empty($_POST["email"])) {
-          $emailErr = "Email is verplicht";
-        } else {
-          $email = ($_POST["email"]);
-        }
-          
+      if (empty($_POST["voornaam"])) {
+        $voornaamErr = "Voornaam is verplicht";
+      } else {
+        $voornaam = ($_POST["voornaam"]);
+      }
       
-        if (empty($_POST["username"])) {
-          $usernameErr = "Username is verplicht";
-        } else {
-          $username = ($_POST["username"]);
-        }
+      if (empty($_POST["achternaam"])) {
+        $achternaamErr = "Achternaam is verplicht";
+      } else {
+        $achternaam = ($_POST["achternaam"]);
+      }
       
-        if (empty($_POST["password"])) {
-          $passwordErr = "Password is verplicht";
-        } else {
-          $password = ($_POST["password"]);
-        }
-
-        if (empty($_POST["repassword"])) {
-          $passwordErr = "Vul je wachtwoord in";
-        } else {
-          $password = ($_POST["repassword"]);
-        }
-
-        if ($_POST["password" !== $_POST["repassword"]]) {
-            $pwMatch = "Wachtwoorden komen niet overeen. Vul opnieuw je wachtwoord in";
-            exit;
-        }
-         // TODO: redirect succcespage/login
-         // TODO: session start
+      if (empty($_POST["email"])) {
+        $emailErr = "Email is verplicht";
+      } else {
+        $email = ($_POST["email"]);
       }
       
       
-?>
+      if (empty($_POST["username"])) {
+        $usernameErr = "Username is verplicht";
+      } else {
+        $username = ($_POST["username"]);
+      }
+      
+      if (empty($_POST["password"])) {
+        $passwordErr = "Password is verplicht";
+      } else {
+        $password = ($_POST["password"]);
+      }
+      
+      if (empty($_POST["repassword"])) {
+        $passwordErr = "Vul je wachtwoord in";
+      } else {
+        $password = ($_POST["repassword"]);
+      }
+  
+      // TODO: redirect succcespage/login
+      // TODO: session start
+          if ($_POST["password"] === $_POST["repassword"]) {
+      
+            $success = "Registratie succesvol";
+      
+          }else {
+            
+            $pwMatch = "Wachtwoorden komen niet overeen. Vul opnieuw je wachtwoord in";
+            
+          }
+    }
+    
+    $db->insertAccount($email, $password);
+    
+    ?>
 
 <html>
 
@@ -79,20 +87,20 @@
             <h3>Sign-up</h3>
             <form action="signup.php" method="post">
                 <label for="voornaam">Voornaam:</label><br>
-                <input required type="text" name="voornaam" id="voornaam" ><span class="error">*<?= $voornaam;?></span><br>
+                <input required type="text" name="voornaam" id="voornaam" ><span class="error"> *<?= $voornaamErr;?></span><br>
                 <label for="tussenvoegsel" >Tussenvoegsel:</label><br>
                 <input type="text" name="tussenvoegsel" id="tussenvoegsel"><br>
                 <label for="achternaam" >Achternaam:</label><br>
-                <input required type="text" name="achternaam" id="achternaam"><span class="error">*<?= $achternaam;?></span><br>
+                <input required type="text" name="achternaam" id="achternaam"><span class="error"> *<?= $achternaamErr;?></span><br>
                 <label for="email">E-mail:</label><br>
                 <input required type="email" name="email" id="email"><span class="error"></span><br>
                 <label for="username">Username:</label><br>
-                <input required pattern="[a-z]{1,15}+@" type="text"  name="username" id="username"><span class="error">*<?= $username;?></span><br>
+                <input required pattern="[a-z]{1,15}+@" type="text"  name="username" id="username"><span class="error"> *<?= $usernameErr;?></span><br>
                 <label for="password" >Password:</label><br>
-                <input required minlength="10" maxlength="20" type="password" name="password" id="password"><span class="error">*<?= $password;?></span><br>
+                <input required minlength="10" maxlength="20" type="password" name="password" id="password"><span class="error"> *<?= $passwordErr;?></span><br>
                 <label for="repassword" >Repeat Password:</label><br>
-                <input required minlength="10" maxlength="20" type="password" name="repassword" id="password"> <span class="error">*<?= $pwMatch;?></span><br>
-                <br>
+                <input required minlength="10" maxlength="20" type="password" name="repassword" id="password"> <span class="error"> *<?= $pwMatch;?></span><br>
+                <br><span><?= $success ?></span>
                 <button type="submit" name="Register" value="Register">Register</button><br>
                 <a href="index.php">Terug naar het begin</a><br>
             </form>
