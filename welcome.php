dc6739760b18b4c5245634d3b0af96dae5bc6801
diego <2095333@talnet.nl>
2020-10-05 14:16:33 +0200

    welkomst pagina alvast aangemaakt voor redirecten na inloggen/registreren

---
+++ welcome.php
@@ -0,0 +1,21 @@
+<?php 
+
+    // TODO:session start 
+
+?>
+
+
+
+<html>
+
+<head>
+    <title>Welkomst pagina</title>
+</head>
+
+<body>
+<fieldset>
+    <p>U bent nu ingelogd! Welkom</p>
+</fieldset>
+</body>
+
+</html>
